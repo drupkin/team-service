3390c5bf9ef9998d99382767f8419800e0382aa3
drupkin <dimitar.rupkin@ovo.com>
2023-01-13 19:24:02 +0200

    init commit

---
+++ src/main/kotlin/bg/dr/chillywe/teamservice/TeamServiceApplication.kt
@@ -0,0 +1,11 @@
+package bg.dr.chillywe.teamservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TeamServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<TeamServiceApplication>(*args)
+}
---
+++ src/test/kotlin/bg/dr/chillywe/teamservice/TeamServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package bg.dr.chillywe.teamservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TeamServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
