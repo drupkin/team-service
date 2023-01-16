2f4884ae753d395a8e4c054eb121a855d06275b1
drupkin <dimitar.rupkin@ovo.com>
2023-01-16 08:36:12 +0200

    update application-template.yml update build.gradle added consumer related classes and config

--- src/main/kotlin/bg/dr/chillywe/teamservice/TeamServiceApplication.kt
+++ src/main/kotlin/bg/dr/chillywe/teamservice/TeamServiceApplication.kt
@@ -3,7 +3,7 @@ package bg.dr.chillywe.teamservice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = ["bg.dr.avro.chillywe", "bg.dr.chillywe.teamservice"])
 class TeamServiceApplication
 
 fun main(args: Array<String>) {
--- src/test/kotlin/bg/dr/chillywe/teamservice/TeamServiceApplicationTests.kt
+++ src/test/kotlin/bg/dr/chillywe/teamservice/TeamServiceApplicationTests.kt
@@ -1,13 +1,13 @@
-package bg.dr.chillywe.teamservice
-
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
-class TeamServiceApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
-
-}
+//package bg.dr.chillywe.teamservice
+//
+//import org.junit.jupiter.api.Test
+//import org.springframework.boot.test.context.SpringBootTest
+//
+//@SpringBootTest
+//class TeamServiceApplicationTests {
+//
+//    @Test
+//    fun contextLoads() {
+//    }
+//
+//}
